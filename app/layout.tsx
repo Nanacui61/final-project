924aead4a125646e94ccd936c4af57168cacd278
Yuqian Cui <yqcui@bu.edu>
2025-08-05 19:32:59 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+// app/layout.tsx
+import type { Metadata } from "next";
+// import StyledComponentsRegistry from "@/lib/registry";
+import { EntityProvider } from "@/context/EntityContext";
+import Nav from "@/components/Nav";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "Treasure Hunt",
+    description: "An eerie treasure hunting experience",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+        <body>
+        {/*<StyledComponentsRegistry>*/}
+        <EntityProvider>
+            <Nav />
+            {children}
+        </EntityProvider>
+        {/*</StyledComponentsRegistry>*/}
+        </body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import styled from 'styled-components';
+
+import Link from 'next/link';
+
+
+const FullScreenWrapper = styled.div`
+    background-color: black;
+    color: #ffcccc;
+    font-family: 'Courier New', monospace;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Wrapper = styled.div`
+    padding: 2rem;
+    text-align: center;
+    color: #ec7a7a;
+    font-family: 'Courier New', monospace;
+    max-width: 700px;
+    margin: 0 auto;
+
+`;
+
+const Title = styled.h1`
+    font-size: 2.5rem;
+    color: #ff4444;
+    margin-bottom: 1rem;
+`;
+
+const Paragraph = styled.p`
+    font-size: 1.5rem;
+    line-height: 1.6;
+    margin-bottom: 2rem;
+`;
+
+
+
+const StyledAnchor = styled.a`
+    background-color: #00ffcc;
+    color: #000;
+    font-size: 1.2rem;
+    padding: 0.75rem 2rem;
+    border: none;
+    border-radius: 6px;
+    cursor: pointer;
+    font-family: monospace;
+
+    &:hover {
+        background-color: #00c3a5;
+    }
+`;
+
+export default function IntroScreen() {
+
+    return (
+        <FullScreenWrapper>
+            <Wrapper>
+                <Title>HELP...</Title>
+                <Paragraph>
+                    You've stumbled upon an ancient website...
+                    Strange symbols flicker across the screen.
+                    A presence is trapped here — something forgotten, buried beneath digital dust.
+                    It reaches out to you through static and whispers...
+                </Paragraph>
+                <Paragraph>
+                    Can you find the hidden entity and set it free before it's trapped forever?
+                </Paragraph>
+                <StyledAnchor href="/New_game">Save Me!!!!!</StyledAnchor>
+
+            </Wrapper>
+        </FullScreenWrapper>
+    );
+}
