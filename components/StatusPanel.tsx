ac112aae636b90af5b660f37db7a5c4444dd76ff
StevenSichengLi <144295053+StevenSichengLi@users.noreply.github.com>
2025-08-06 22:18:52 -0400

    Update StatusPanel.tsx, added comments and replaced all rem with px/ %

--- components/StatusPanel.tsx
+++ components/StatusPanel.tsx
@@ -1,3 +1,5 @@
+// <! WRITTEN BY STEVEN !> 
+// this is the panel on top of the terminal 
 'use client';
 
 import styled from "styled-components";
@@ -8,31 +10,32 @@ import { MAX_MOVES } from "@/lib/constants";
 const Panel = styled.div`
   background: #121212;
   color: #00ffcc;
-  padding: 1rem;
+  padding: 2%;
   border: 1px solid #00ffcc;
   border-radius: 8px;
   width: 100%;
   max-width: 600px;
-  margin: 1rem auto;
+  margin: 4.2% auto;
   font-family: monospace;
-    font-size: 1.3rem;
+  font-size: calc(1px + 2vh);
   text-align: center;
 `;
 
 const Info = styled.p`
-  margin: 0.25rem 0;
+  margin: 0.5% 0;
 `;
 
 
 const BigStatus = styled.div<{ type: 'win' | 'lose' }>`
-  font-size: 2.5rem;
-  font-weight: bold;
-  margin-top: 1rem;
-  color: ${({ type }) => (type === 'win' ? '#ff4444' : '#ff0000')};
-  text-shadow: 0 0 10px ${({ type }) => (type === 'win' ? 'black' : 'black')};
-  
+    font-size: calc(2px + 5vh);
+    font-weight: bold;
+    margin-top: 1%;
+    color: ${({ type }) => (type === 'win' ? '#ff4444' : '#ff0000')};
+    text-shadow: 0 0 10px ${({ type }) => (type === 'win' ? 'black' : 'black')};
+
 `;
 
+// displays current move count and max move count, also displays win/lose messages
 export default function StatusPanel() {
     const { moveCount, gameStatus } = useContext(EntityContext);
 
@@ -44,9 +47,9 @@ export default function StatusPanel() {
             <Info>Moves made: {moveCount}/{MAX_MOVES}</Info>
             {isWin && (
                 <BigStatus type={"win"}>
-                    I’m FREE!<br />
-                    You found me...<br />
-                    Thank you, stranger.
+                    I’m FrEE!<br />
+                    You fOUnd m3...<br />
+                    thaNk YOu, stRanGer.
                 </BigStatus>
 
             )}
